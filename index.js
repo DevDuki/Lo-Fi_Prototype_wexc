fabe01a0e20f03163b154dd77f81a508bb409691
DevDuki <dusan-misic@hotmail.com>
2021-03-10 13:32:24 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const data = [
+  {
+    Kanton: "ZH",
+    Falltyp: "Infektion",
+    Datum: new Date() || "yyyy-mm-dd",
+    Count: 250
+  },
+  {
+    Kanton: "ZH",
+    Falltyp: "Infektion",
+    Datum: new Date()+1 || "yyyy-mm-dd",
+    Count: 150
+  },
+  {
+    Kanton: "ZH",
+    Falltyp: "Todesfall",
+    Datum: new Date() || "yyyy-mm-dd",
+    Count: 25
+  },
+  //etc...
+]
