3b599a6e2586c1a624f9b41024e3696fa28a4e1e
DevDuki <dusan-misic@hotmail.com>
2021-03-12 20:00:33 +0100

    optimisation in html and added more sample data

--- index.js
+++ index.js
@@ -1,20 +1,198 @@
 const data = [
   {
-    Kanton: "ZH",
+    Kanton: "Zürich",
     Falltyp: "Infektion",
-    Datum: new Date() || "yyyy-mm-dd",
+    Datum: "2021-03-01", // yyyy-mm-dd
     Count: 250
   },
   {
-    Kanton: "ZH",
+    Kanton: "Zürich",
     Falltyp: "Infektion",
-    Datum: new Date() + 1 || "yyyy-mm-dd",
-    Count: 150
+    Datum: "2021-02-28",
+    Count: 225
   },
   {
-    Kanton: "ZH",
-    Falltyp: "Todesfall",
-    Datum: new Date() || "yyyy-mm-dd",
-    Count: 25
+    Kanton: "Zürich",
+    Falltyp: "Infektion",
+    Datum: "2021-02-27",
+    Count: 100
   },
+  {
+    Kanton: "Zürich",
+    Falltyp: "Infektion",
+    Datum: "2021-02-26",
+    Count: 190
+  },
+  {
+    Kanton: "Zürich",
+    Falltyp: "Todesfall",
+    Datum: "2021-03-01",
+    Count: 50
+  },
+  {
+    Kanton: "Zürich",
+    Falltyp: "Todesfall",
+    Datum: "2021-02-28",
+    Count: 10
+  },
+  {
+    Kanton: "Zürich",
+    Falltyp: "Todesfall",
+    Datum: "2021-02-27",
+    Count: 12
+  },
+  {
+    Kanton: "Zürich",
+    Falltyp: "Todesfall",
+    Datum: "2021-02-26",
+    Count: 28
+  },
+  
+  {
+    Kanton: "Zug",
+    Falltyp: "Infektion",
+    Datum: "2021-03-01",
+    Count: 170
+  },
+  {
+    Kanton: "Zug",
+    Falltyp: "Infektion",
+    Datum: "2021-02-28",
+    Count: 162
+  },
+  {
+    Kanton: "Zug",
+    Falltyp: "Infektion",
+    Datum: "2021-02-27",
+    Count: 111
+  },
+  {
+    Kanton: "Zug",
+    Falltyp: "Infektion",
+    Datum: "2021-02-26",
+    Count: 96
+  },
+  {
+    Kanton: "Zug",
+    Falltyp: "Todesfall",
+    Datum: "2021-03-01",
+    Count: 12
+  },
+  {
+    Kanton: "Zug",
+    Falltyp: "Todesfall",
+    Datum: "2021-02-28",
+    Count: 9
+  },
+  {
+    Kanton: "Zug",
+    Falltyp: "Todesfall",
+    Datum: "2021-02-27",
+    Count: 5
+  },
+  {
+    Kanton: "Zug",
+    Falltyp: "Todesfall",
+    Datum: "2021-02-26",
+    Count: 7
+  },
+
+  {
+    Kanton: "Basel",
+    Falltyp: "Infektion",
+    Datum: "2021-03-01",
+    Count: 223
+  },
+  {
+    Kanton: "Basel",
+    Falltyp: "Infektion",
+    Datum: "2021-02-28",
+    Count: 198
+  },
+  {
+    Kanton: "Basel",
+    Falltyp: "Infektion",
+    Datum: "2021-02-27",
+    Count: 142
+  },
+  {
+    Kanton: "Basel",
+    Falltyp: "Infektion",
+    Datum: "2021-02-26",
+    Count: 203
+  },
+  {
+    Kanton: "Basel",
+    Falltyp: "Todesfall",
+    Datum: "2021-03-01",
+    Count: 12
+  },
+  {
+    Kanton: "Basel",
+    Falltyp: "Todesfall",
+    Datum: "2021-02-28",
+    Count: 9
+  },
+  {
+    Kanton: "Basel",
+    Falltyp: "Todesfall",
+    Datum: "2021-02-27",
+    Count: 8
+  },
+  {
+    Kanton: "Basel",
+    Falltyp: "Todesfall",
+    Datum: "2021-02-26",
+    Count: 19
+  },
+
+  {
+    Kanton: "Schaffhausen",
+    Falltyp: "Infektion",
+    Datum: "2021-03-01",
+    Count: 123
+  },
+  {
+    Kanton: "Schaffhausen",
+    Falltyp: "Infektion",
+    Datum: "2021-02-28",
+    Count: 182
+  },
+  {
+    Kanton: "Schaffhausen",
+    Falltyp: "Infektion",
+    Datum: "2021-02-27",
+    Count: 122
+  },
+  {
+    Kanton: "Schaffhausen",
+    Falltyp: "Infektion",
+    Datum: "2021-02-26",
+    Count: 179
+  },
+  {
+    Kanton: "Schaffhausen",
+    Falltyp: "Todesfall",
+    Datum: "2021-03-01",
+    Count: 6
+  },
+  {
+    Kanton: "Schaffhausen",
+    Falltyp: "Todesfall",
+    Datum: "2021-02-28",
+    Count: 1
+  },
+  {
+    Kanton: "Schaffhausen",
+    Falltyp: "Todesfall",
+    Datum: "2021-02-27",
+    Count: 0
+  },
+  {
+    Kanton: "Schaffhausen",
+    Falltyp: "Todesfall",
+    Datum: "2021-02-26",
+    Count: 7
+  },
+
 ]
