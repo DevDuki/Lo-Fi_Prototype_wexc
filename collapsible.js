c771242981c1d10f21ba5a0bd3770c30c6e53500
Dierk Koenig <dierk.koenig@canoo.com>
2021-03-31 12:59:48 +0200

    refactor remove duplication

--- collapsible.js
+++ collapsible.js
@@ -2,30 +2,17 @@ const totalCases    = document.querySelectorAll('.total-cases-title');
 const infectedCases = document.querySelectorAll('.infected-title');
 const deathCases    = document.querySelectorAll('.death-title');
 
-totalCases.forEach(element => {
+const toggleOnClick = element => {
     element.addEventListener('click', event => {
         event.stopPropagation(); // otherwise we cannot toggle nested ULs
         toggleCollapse(element);
     });
-});
+};
 
+totalCases   .forEach(element => toggleOnClick(element));
+infectedCases.forEach(element => toggleOnClick(element));
+deathCases   .forEach(element => toggleOnClick(element));
 
-infectedCases.forEach(total => {
-    total.addEventListener('click', (e) => {
-        e.stopPropagation()
-
-        toggleCollapse(e.target)
-    })
-})
-
-
-deathCases.forEach(total => {
-    total.addEventListener('click', (e) => {
-        e.stopPropagation()
-
-        toggleCollapse(e.target)
-    })
-})
 
 const toggleCollapse = (element) => {
     const siblingList = element.nextElementSibling
