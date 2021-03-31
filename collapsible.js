d1fde4b46abd6459755c014e0c2c6a17d163e79f
Dierk Koenig <dierk.koenig@canoo.com>
2021-03-31 13:05:36 +0200

    refactor remove duplication (foreach)

--- collapsible.js
+++ collapsible.js
@@ -2,19 +2,21 @@ const totalCases    = document.querySelectorAll('.total-cases-title');
 const infectedCases = document.querySelectorAll('.infected-title');
 const deathCases    = document.querySelectorAll('.death-title');
 
-const toggleOnClick = element => {
-    element.addEventListener('click', event => {
-        event.stopPropagation(); // otherwise we cannot toggle nested ULs
-        toggleCollapse(element);
-    });
-};
+const toggleOnClick = elements =>
+    elements.forEach( element =>
+        element.addEventListener('click', event => {
+            event.stopPropagation(); // otherwise we cannot toggle nested ULs
+            toggleCollapse(element);
+        })
+    )
+;
 
-totalCases   .forEach(element => toggleOnClick(element));
-infectedCases.forEach(element => toggleOnClick(element));
-deathCases   .forEach(element => toggleOnClick(element));
+toggleOnClick(totalCases   );
+toggleOnClick(infectedCases);
+toggleOnClick(deathCases   );
 
 
-const toggleCollapse = (element) => {
+const toggleCollapse = element => {
     const siblingList = element.nextElementSibling
     const display = siblingList.style.display
 
